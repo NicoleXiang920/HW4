ac96e630460a3997c2a306056dd2588b9659ee6a
NicoleXiang920 <144865830+NicoleXiang920@users.noreply.github.com>
2024-11-04 10:33:11 -0800

    Update fft.py

--- fft.py
+++ fft.py
@@ -16,9 +16,9 @@ xf = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
 fig, ax = plt.subplots()
 
 #Uncomment this section to plot the signal itself
-ax.plot(x,y)
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
+# ax.plot(x,y)
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
 
 #uncomment this section to plot the fourier transform output using the FFT algorithm
 ax.plot(xf, 2.0 / N * np.abs(yf[:N // 2]))
